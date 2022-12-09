0792f66e65d72c875449333f0a9ea18b003d4c42
KIM DONGHYEON <kehdgus4152@naver.com>
2022-12-09 10:55:02 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer =[
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name' : '김동현',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name' : '김기동',
+    'birthday' : '960305',
+    'gender' : '남자',
+    'job' : '프로그래머'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/any',
+    'name' : '김서방',
+    'birthday' : '981111',
+    'gender' : '남자',
+    'job' : '백수'
+  },
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {
+          customer.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
